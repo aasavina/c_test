8ba0b5449a8708e43299cfb6cbf58aeec7be0f33
Anastasia <asavina@yandex.ru>
2022-09-18 00:06:41 +0300

    else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+Console.WriteLine( "Ура этоже маша");
+}
+else
+{
+Console.Write("Привет незнакомец ");
+Console.WriteLine(username);
+}
