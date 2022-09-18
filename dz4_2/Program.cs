5ec30a67625135213a01a3cf7dade7178effe381
Anastasia <asavina@yandex.ru>
2022-09-18 16:58:43 +0300

    вводим 3 любых числаб чтоб не повторять 1 задачу

---
+++ dz4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число1");
+int numberA =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2");
+int numberB =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число3");
+int numberC =  Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max =numberB;
+if (numberC > max) max =numberC;
+Console.WriteLine(max);
