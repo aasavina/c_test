fc2b107fc1b4cd63f9a9cebdd02368851c193a8e
Anastasia <asavina@yandex.ru>
2022-09-18 17:57:42 +0300

    не помню зачем $

---
+++ dz8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+double number = Convert.ToDouble(Console.ReadLine());
+double count = 1;
+while (count <= number)
+{
+ if (count % 2 ==0)
+ {
+    Console.Write($"{count},");
+ }
+count ++;
+}
