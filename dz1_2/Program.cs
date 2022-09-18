ba67e4be11dc3137528a5cfbc7af78807bb04b86
Anastasia <asavina@yandex.ru>
2022-09-18 16:50:24 +0300

    вводим любые 2 числа, чтоб не повторять 1 вариант  с другими числами

---
+++ dz1_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число1");
+int numberA =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2");
+int numberB =  Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max =numberB;
+Console.WriteLine(max);
