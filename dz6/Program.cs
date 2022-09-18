b08b5aa26482d1b7b0a77a9c46f3b2e2c487cc01
Anastasia <asavina@yandex.ru>
2022-09-18 17:21:58 +0300

    задача 6

---
+++ dz6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое положительное или отрицательное число");
+double Number = Convert.ToDouble(Console.ReadLine());
+if (Number%2 ==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
