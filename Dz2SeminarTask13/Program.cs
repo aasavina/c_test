0f77077cc1bf592086cc3290e7255d21b0928957
Anastasia <asavina@yandex.ru>
2022-09-21 23:44:12 +0300

    третье число

---
+++ Dz2SeminarTask13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 > 0)
+{
+    while (number>1000)
+    {
+        number=number/10;
+    }
+int third =number %10;
+Console.WriteLine($"{third}");
+}
+else
+{
+    Console.WriteLine(" нет третьего числа");
+}
