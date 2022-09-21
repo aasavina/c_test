6b8892c0571ae85bda33ff3924a86dc098f84fa6
Anastasia <asavina@yandex.ru>
2022-09-21 22:10:31 +0300

    задача 10

---
+++ Dz2SeminarTask10/Program.cs
@@ -0,0 +1,10 @@
+﻿int seconddigit(int num)
+{
+ int result =(num /10) %10 ;
+ return result;
+}
+
+Console.WriteLine("Введите цифру XXX");
+int num = Convert.ToInt32(Console.ReadLine());
+int x =seconddigit(num);
+Console.WriteLine($"Вторая цифра из числа {num}  это {x}");
