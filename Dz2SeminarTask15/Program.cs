f338aaf4567352efd68614714c04a105fa504d2b
Anastasia <asavina@yandex.ru>
2022-09-23 20:31:39 +0300

    задача 15

---
+++ Dz2SeminarTask15/Program.cs
@@ -0,0 +1,16 @@
+﻿void holiday (int weekday) 
+{
+  if (weekday == 6 || weekday == 7) 
+  {
+  Console.WriteLine("да");
+  }
+  else if (weekday < 1 || weekday > 7) 
+  {
+    Console.WriteLine("Вы ввели некорректное число");
+  }
+  else Console.WriteLine("нет");
+}
+
+Console.WriteLine("Введите номер дня недели: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+holiday(weekday);
