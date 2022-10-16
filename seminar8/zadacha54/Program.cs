72565e3e51b1461883301dd722e43948e2b41b3d
Anastasia <asavina@yandex.ru>
2022-10-16 22:15:33 +0300

    2 задачи

---
+++ seminar8/zadacha54/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+int[,]Createarray(int rows,int columns)
+{
+    int[,]newarray= new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++)
+       {
+        newarray[i,j]=Convert.ToInt32(new Random().Next(1,10));
+        
+       } 
+    }
+    return newarray;
+
+}
+
+void Showarray(int[,]incomingarray)
+{
+    for (int i = 0; i < incomingarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < incomingarray.GetLength(1); j++)
+        {
+            Console.Write(incomingarray[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,]Sortarray(int[,]notsortedarray)
+{
+ for (int i = 0; i < notsortedarray.GetLength(0); i++)
+ {
+    for (int j = 0; j < notsortedarray.GetLength(1)-1; j++)
+    {
+       
+       for (int x = 0; x < notsortedarray.GetLength(1)-1; x++)
+       {
+        int tempmin=0;
+        if (notsortedarray[i,x]<notsortedarray[i,x+1]) 
+        {
+        tempmin = notsortedarray[i,x];
+        notsortedarray[i,x]=notsortedarray[i,x+1];
+        notsortedarray[i,x+1] = tempmin;
+        }
+       }
+
+    }
+ }
+ return notsortedarray;
+}
+int[,]array2d=Createarray(3,4);
+Showarray(array2d);
+Sortarray(array2d);
+Console.WriteLine();
+Showarray(array2d);
---
+++ seminar8/zadacha56/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. arrayfromuser
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int[,]Createarray(int rows,int columns)
+{
+    int[,]newarray= new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+       for (int j = 0; j < columns; j++)
+       {
+        newarray[i,j]=Convert.ToInt32(new Random().Next(1,10));
+        
+       } 
+    }
+    return newarray;
+
+}
+
+void Showarray(int[,]incomingarray)
+{
+    for (int i = 0; i < incomingarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < incomingarray.GetLength(1); j++)
+        {
+            Console.Write(incomingarray[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Findmaxrow(int[,]arrayfromuser)
+{
+    int minrowsumm = 1000;
+    int Rownumber = 0;
+            
+            for (int i = 0; i < arrayfromuser.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < arrayfromuser.GetLength(1); j++)
+                {
+                   sum = sum + arrayfromuser[i, j];
+                }
+                Console.WriteLine($"Сумма {sum} для ряда {i+1}");
+                if (sum < minrowsumm)
+                {
+                    minrowsumm = sum;
+                    Rownumber =i+1;
+                }
+            }
+            Console.WriteLine();
+    Console.WriteLine( $"Строка с минимальной суммой {Rownumber}, а сумма равна {minrowsumm}");
+}
+
+int[,]newarray= Createarray(4,4);
+Showarray(newarray);
+Console.WriteLine();
+Findmaxrow(newarray);
+
